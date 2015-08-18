6ddea8bcfcef5913a0ee1ef441e5445bfa8078af
efranklin25 <eddie.franklin20@gmail.com>
2015-08-17 20:23:36 -0700

    Added Scheduler
    Update to DB daily

--- execute.py
+++ execute.py
@@ -16,21 +16,33 @@ from umbrellaPM import umbrella_pm
 from campusconnectionPM import campusconnection_pm
 from valleyinvestmentPM import valleyinvestment_pm
 
-acorn_pm()
-bell_pm()
-bell_campus_pm()
-campusconnection_pm()
-chinook_pm()
-emerald_pm()
-full_house_pm()
-jennings_group_pm()
-mallard_pm()
-metco_pm()
-northwoods_pm()
-pioneer_pm()
-preferred_nw_pm()
-premier_pm()
-principle_pm()
-trio_pm()
-umbrella_pm()
-valleyinvestment_pm()
+from datetime import datetime
+from threading import Timer
+
+def update():
+	acorn_pm()
+	bell_pm()
+	bell_campus_pm()
+	campusconnection_pm()
+	chinook_pm()
+	emerald_pm()
+	full_house_pm()
+	jennings_group_pm()
+	mallard_pm()
+	metco_pm()
+	northwoods_pm()
+	pioneer_pm()
+	preferred_nw_pm()
+	premier_pm()
+	principle_pm()
+	trio_pm()
+	umbrella_pm()
+	valleyinvestment_pm()
+
+now = datetime.today()
+change = now.replace(day=now.day+1, hour=1, minute=0, second=0, microsecond=0)
+difference = change - now
+seconds = difference.seconds+1
+
+t = Timer(seconds, update)
+t.start()
